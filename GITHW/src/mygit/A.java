c00d48a27fa72b12f6d0e13202f04fa0aa705f94
codingmonster7 <peter1256@skuniv.ac.kr>
2023-12-26 10:44:41 +0900

    A.java, C.java dev_c로 branch

--- GITHW/src/mygit/A.java
+++ GITHW/src/mygit/A.java
@@ -4,7 +4,8 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("This is A.java first version");
-		System.out.println("This is A.java Master branch");
+		System.out.println("This is A.java second version , And dev_c branch.");
+		
 	}
 
 }
--- GITHW/src/mygit/C.java
+++ GITHW/src/mygit/C.java
@@ -4,7 +4,7 @@ public class C {
 
 	public static void main(String[] args) {
 		System.out.println("This is C.java first version");
-
+		System.out.println("This is C.java second version , And dev_c branch.");
 	}
 
 }
