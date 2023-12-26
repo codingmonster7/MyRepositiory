954320e6768f3ce229238b34478532bfebbafb54
codingmonster7 <peter1256@skuniv.ac.kr>
2023-12-26 10:42:11 +0900

    A.java, B.java 를 두번째 버전으로 바꿈

--- GITHW/src/mygit/A.java
+++ GITHW/src/mygit/A.java
@@ -4,7 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("This is A.java first version");
-
+		System.out.prinlnt("This is A.java second version");
 	}
 
 }
--- GITHW/src/mygit/B.java
+++ GITHW/src/mygit/B.java
@@ -4,7 +4,8 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("This is B.java first version");
-
+		System.out.println("This is B.java second version");
+		
 	}
 
 }
